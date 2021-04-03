19c2e91acba514eff1a2e2e3ea9a8eecdf44fb41
Eduardo Lima <edulima2412@gmail.com>
2021-04-03 11:31:42 -0300

    criacao do service de login

---
+++ src/Api.Service/Services/LoginService.cs
@@ -0,0 +1,30 @@
+using System.Threading.Tasks;
+using Api.Domain.Entities;
+using Api.Domain.Interfaces.Services.User;
+using Api.Domain.Repository;
+
+namespace Api.Service.Services
+{
+    public class LoginService : ILoginService
+    {
+        private readonly IUserRepository _repository;
+
+        public LoginService(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<object> FindByLogin(UserEntity user)
+        {
+            if (user != null && !string.IsNullOrWhiteSpace(user.Email))
+            {
+                return await _repository.FindByLogin(user.Email);
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+    }
+}
