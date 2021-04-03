e4625237982090bbfae21fbd1862e1066f7fe035
Eduardo Lima <edulima2412@gmail.com>
2021-04-03 14:40:35 -0300

    criacao dos user dtos

--- src/Api.Domain/Dtos/LoginDto.cs
+++ src/Api.Domain/Dtos/LoginDto.cs
@@ -5,7 +5,7 @@ namespace Api.Domain.Dtos
     public class LoginDto
     {
         [Required(ErrorMessage = "Email é obrigatório.")]
-        [EmailAddress(ErrorMessage = "Email em formato inválido.")]
+        [EmailAddress(ErrorMessage = "Email no formato inválido.")]
         [MaxLength(100, ErrorMessage = "Email deve ter no máximo 100 caracteres.")]
         public string Email { get; set; }
     }
