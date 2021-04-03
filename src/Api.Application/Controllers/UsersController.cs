70ee6bd7977ec301b2f4125525cdb6757b32f1b2
Eduardo Lima <edulima2412@gmail.com>
2021-04-03 08:25:35 -0300

    criacao do getall no user controller

--- src/Api.Application/Controllers/UsersController.cs
+++ src/Api.Application/Controllers/UsersController.cs
@@ -1,7 +1,30 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Api.Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Api.Application.Controllers
 {
-    public class UsersController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
     {
-        
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll([FromServices] IUserService service)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                return Ok(await service.GetAll());
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
     }
 }
