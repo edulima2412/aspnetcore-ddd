83dd5f414b526c529614d340f86a913c620981eb
Eduardo Lima <edulima2412@gmail.com>
2021-04-03 06:57:33 -0300

    criacao do contextFactory

---
+++ src/Api.Data/Context/ContextFactory.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Api.Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            // Usado para criar as migrações
+            var connectionString = "Server=localhost;Port=3306;Database=dbAPI;Uid=root;Pwd=admin123";
+            var optionBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionBuilder.UseMySql(connectionString);
+            return new MyContext(optionBuilder.Options);
+        }
+    }
+}
