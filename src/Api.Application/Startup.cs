11ac8c0ba7576350acb06def9fe225c91e4a050f
Eduardo Lima <edulima2412@gmail.com>
2021-04-03 09:59:08 -0300

    implementando swagger

--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -1,9 +1,11 @@
+using System;
 using Api.CrossCutting.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace Application
 {
@@ -23,6 +25,22 @@ namespace Application
             ConfigureRepository.ConfigureDependenciesRepository(services);
 
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "API com AspNetCore",
+                    Description = "API AspNetCore com arquitetura DDD",
+                    TermsOfService = new Uri("https://edulima2412.github.io"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Eduardo Lima",
+                        Email = "edulima2412@gmail.com"
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,6 +50,13 @@ namespace Application
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "AspNetCore");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseRouting();
 
