cb863ed5a45e41f11849fffd0841c435d7f47fa0
Eduardo Lima <edulima2412@gmail.com>
2021-04-09 10:28:15 -0300

    criacao dos testes service uf

---
+++ src/Api.Service.Test/Municipio/MunicipioTestes.cs
@@ -0,0 +1,101 @@
+﻿using Api.Domain.Dtos.Municipio;
+using Api.Domain.Dtos.Uf;
+using System;
+using System.Collections.Generic;
+
+namespace Api.Service.Test.Municipio
+{
+    public class MunicipioTestes
+    {
+        public static Guid Id { get; set; }
+        public static string Nome { get; set; }
+        public static string NomeAlterado { get; set; }
+        public static int CodIBGE { get; set; }
+        public static int CodIBGEAlterado { get; set; }
+        public static Guid UfId { get; set; }
+
+        public List<MunicipioDto> listaDto = new List<MunicipioDto>();
+        public MunicipioDto municipioDto;
+        public MunicipioDtoCompleto municipioDtoCompleto;
+        public MunicipioDtoCreate municipioDtoCreate;
+        public MunicipioDtoCreateResult municipioDtoCreateResult;
+        public MunicipioDtoUpdate municipioDtoUpdate;
+        public MunicipioDtoUpdateResult municipioDtoUpdateResult;
+
+        public MunicipioTestes()
+        {
+            Id = Guid.NewGuid();
+            Nome = Faker.Address.City();
+            NomeAlterado = Faker.Address.City();
+            CodIBGE = Faker.RandomNumber.Next(1, 10000);
+            CodIBGEAlterado = Faker.RandomNumber.Next(1, 10000);
+            UfId = Guid.NewGuid();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var dto = new MunicipioDto
+                {
+                    Id = Guid.NewGuid(),
+                    Nome = Faker.Address.City(),
+                    CodIBGE = Faker.RandomNumber.Next(1, 10000),
+                    UfId = Guid.NewGuid()
+                };
+                listaDto.Add(dto);
+            }
+
+            municipioDto = new MunicipioDto
+            {
+                Id = Id,
+                Nome = Nome,
+                CodIBGE = CodIBGE,
+                UfId = UfId
+            };
+
+            municipioDtoCompleto = new MunicipioDtoCompleto
+            {
+                Id = Id,
+                Nome = Nome,
+                CodIBGE = CodIBGE,
+                UfId = UfId,
+                Uf = new UfDto
+                {
+                    Id = Guid.NewGuid(),
+                    Sigla = Faker.Address.UsState().Substring(1, 3),
+                    Nome = Faker.Address.UsState()
+                }
+            };
+
+            municipioDtoCreate = new MunicipioDtoCreate
+            {
+                Nome = Nome,
+                CodIBGE = CodIBGE,
+                UfId = UfId
+            };
+
+            municipioDtoCreateResult = new MunicipioDtoCreateResult
+            {
+                Id = Id,
+                Nome = Nome,
+                CodIBGE = CodIBGE,
+                UfId = UfId
+            };
+
+            municipioDtoUpdate = new MunicipioDtoUpdate
+            {
+                Id = Id,
+                Nome = NomeAlterado,
+                CodIBGE = CodIBGEAlterado,
+                UfId = UfId
+            };
+
+            municipioDtoUpdateResult = new MunicipioDtoUpdateResult
+            {
+                Id = Id,
+                Nome = NomeAlterado,
+                CodIBGE = CodIBGEAlterado,
+                UfId = UfId,
+                UpdateAt = DateTime.Now
+            };
+        }
+    }
+}
---
+++ src/Api.Service.Test/Uf/QuandoForExecutadoGet.cs
@@ -0,0 +1,35 @@
+﻿using Api.Domain.Dtos.Uf;
+using Api.Domain.Interfaces.Services.Uf;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Service.Test.Uf
+{
+    public class QuandoForExecutadoGet : UfTestes
+    {
+        private IUfService _service;
+        private Mock<IUfService> _serviceMock;
+
+        [Fact(DisplayName = "É possivel executar o metodo GET.")]
+        public async Task E_Possivel_Executar_Get()
+        {
+            _serviceMock = new Mock<IUfService>();
+            _serviceMock.Setup(m => m.Get(Id)).ReturnsAsync(ufDto);
+            _service = _serviceMock.Object;
+
+            var result = await _service.Get(Id);
+            Assert.NotNull(result);
+            Assert.True(result.Id == Id);
+            Assert.Equal(Nome, result.Nome);
+
+            _serviceMock = new Mock<IUfService>();
+            _serviceMock.Setup(m => m.Get(It.IsAny<Guid>())).Returns(Task.FromResult((UfDto)null));
+            _service = _serviceMock.Object;
+
+            var record = await _service.Get(Id);
+            Assert.Null(record);
+        }
+    }
+}
