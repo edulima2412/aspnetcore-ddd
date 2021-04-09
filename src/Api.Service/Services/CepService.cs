c98bcf73088f4ecb942e8199be834395ffdb25e4
Eduardo Lima <edulima2412@gmail.com>
2021-04-09 08:05:47 -0300

    criacao dos services do cep, mun e uf

---
+++ src/Api.Service/Services/CepService.cs
@@ -0,0 +1,60 @@
+﻿using Api.Domain.Dtos.Cep;
+using Api.Domain.Entities;
+using Api.Domain.Interfaces.Services.Cep;
+using Api.Domain.Models;
+using Api.Domain.Repository;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Services
+{
+    class CepService : ICepService
+    {
+        private readonly ICepRepository _repository;
+        private readonly IMapper _mapper;
+
+        public CepService(ICepRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            return await _repository.DeleteAsync(id);
+        }
+
+        public async Task<CepDto> Get(Guid id)
+        {
+            var entity = await _repository.SelectAsync(id);
+            return _mapper.Map<CepDto>(entity);
+        }
+
+        public async Task<CepDto> Get(string cep)
+        {
+            var entity = await _repository.SelectAsync(cep);
+            return _mapper.Map<CepDto>(entity);
+        }
+
+        public async Task<CepDtoCreateResult> Post(CepDtoCreate cep)
+        {
+            var model = _mapper.Map<CepModel>(cep);
+            var entity = _mapper.Map<CepEntity>(model);
+            var result = await _repository.InsertAsync(entity);
+
+            return _mapper.Map<CepDtoCreateResult>(result);
+        }
+
+        public async Task<CepDtoUpdateResult> Put(CepDtoUpdate cep)
+        {
+            var model = _mapper.Map<CepModel>(cep);
+            var entity = _mapper.Map<CepEntity>(model);
+            var result = await _repository.InsertAsync(entity);
+
+            return _mapper.Map<CepDtoUpdateResult>(result);
+        }
+    }
+}
